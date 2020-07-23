2aad0bc9eaef8cdc7c4d7dd4992de5de283367a8
MiGueAJM9724 <miguejm@outlook.es>
2020-07-23 18:44:12 -0500

    Modulo 3 DB

--- Sentencias.sql
+++ Sentencias.sql
@@ -1,4 +1,7 @@
-CREATE TABLE autor(
+DROP DATABASE IF EXISTS libreria; --Condicionar sentencias
+CREATE TABLE IF NOT EXISTS libreria;
+USE libreria;
+CREATE TABLE IF NOT EXISTS autor(
   autor_id INT,
   nombre VARCHAR(25),
   apellido VARCHAR(25),
@@ -6,3 +9,23 @@ CREATE TABLE autor(
   fecha_nacimiento DATE,
   pais_origen VARCHAR(15)
 );
+-- SHOW TABLES;
+-- SELECT DATABSE();
+-- DROP TABLE autor;
+-- SHOW COLUMNS FROM autor;
+-- DESC autor; abrebiatura de show columns FROM
+-- CREATE TABLE usuario LIKE autor; crear tablas apartir de otras
+-- INSERTAR REGISTROS
+ INSERT INTO autor(autor_id, nombre, apellido, genero, fecha_nacimiento, pais_origen)
+ VALUES(1, 'Test autor', 'Test autor','M', '2020-07-23', 'México');
+
+ /*
+ Para ejecurar archivos .SQL
+1° Forma
+ mysql -u root -p < CursoDB/sentencias.sql
+2° Forma
+ SOURCE CursoDB/sentencias.sql;
+ */
+
+-- CONSULTAS RAPIDAS
+-- mysql -u root -p libreria -e 'SELECT * FROM autor;'
